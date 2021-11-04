08ddbd90700d65afbabb4bfcdcdb21523c6731ea
ostus <ooostus@gmail.com>
2021-11-04 18:53:04 +0900

    Add a download service and duplicate check when uploading

--- client/client.c
+++ client/client.c
@@ -62,43 +62,72 @@ int main(int argc, char* argv[]) {
 			exit(1);
 		}
 		if(!strcmp(temp, "reset")) {	
-			printf("====filelist reset====\n");
+			printf("\n====filelist reset====\n");
 	        	printf("%s", buf);
 		}
 		else if(!strcmp(temp, "upload")) {
-			printf("====upload service====\n");
+			printf("\n====upload service====\n");
 			printf("%s", buf);
 			memset(buf, '\0', BUFSIZ);
 			memset(temp, '\0', BUFSIZ);
-			scanf("%s", buf);
+			scanf("%s", buf); // 업로드할 파일명 buf에 저장
 				
-			if((fp = fopen(buf, "r")) == NULL) {
+			if((fp = fopen(buf, "r")) == NULL) { // 파일 오픈
 				perror("fopen");
 				exit(1);
 			}
-			strcat(buf, "\n");
-			while((fread(temp, sizeof(char), 1, fp)) > 0) {
+			strcat(buf, "\n"); // 파일명, 파일내용을 구분하기 위한 개행문자 추가
+			while((fread(temp, sizeof(char), 1, fp)) > 0) { // 파일 내용 buf에 추가
 				strcat(buf, temp);
 			}
 			
-			if(send(clt_sock, buf, strlen(buf)+1, 0) == -1) {
+			if(send(clt_sock, buf, strlen(buf)+1, 0) == -1) { // 파일명+파일내용 서버로 전송
 				perror("send of file");
 				exit(1);
 			}
 			fclose(fp);
-			if(recv(clt_sock, buf, sizeof(buf), 0) == -1) {
+			if(recv(clt_sock, buf, sizeof(buf), 0) == -1) { // 성공 여부 메세지 받기
 				perror("recv of success message");
 				exit(1);
 			}
-			printf("%s\n", buf);
+			if(!strcmp(buf, "exist")) { // 파일명이 이미 존재하는 경우, 업로드 실패
+				printf("filename exists on the server(upload failed)\n");
+			}
+			else { // 업로드 완료
+				printf("%s\n", buf);
+			}
 		}
 		else if(!strcmp(temp, "download")) {
-			printf("====download service====\n");
+			printf("\n====download service====\n");
+	        printf("%swhich file? : ", buf);
+			memset(buf, '\0', BUFSIZ);
+			scanf("%s", buf); // 다운로드할 파일 선택
+			
+			if(send(clt_sock, buf, strlen(buf)+1, 0) == -1) { // 파일명 전송
+				perror("send of filename");
+				exit(1);
+			}
+			memset(buf, '\0', BUFSIZ);
+			if(recv(clt_sock, buf, sizeof(buf), 0) == -1) { // 파일 내용 받기
+				perror("recv of file content");
+				exit(1);
+			}
+			memset(temp, '\0', BUFSIZ);
+			printf("input file path(include file name) : "); // 저장할 파일 경로 입력
+			scanf("%s", temp);
+			if((fp = fopen(temp, "w")) == NULL) { // 저장할 파일 오픈
+				perror("fopen");
+				exit(1);
+			}
+			fputs(buf, fp); // 파일에 내용 입력
+			printf("file download completed ...\n"); // 저장 완료
+			fclose(fp);
+
 		}
 		else {
 			printf("%s\n", buf);
 		}
-		printf("end\n");
+		printf("end\n\n");
 
 	}
 	close(clt_sock);
--- server.c
+++ server.c
@@ -14,6 +14,7 @@
 #define FILELIST_TXT "./filelist.txt"
 
 void filelist(char* buf);
+int fileexistcheck(char *buf);
 
 int main() {
 	struct sockaddr_in svr_sin, clt_sin;
@@ -100,31 +101,70 @@ int main() {
 							strcpy(file_name, temp_file);
 							temp_file = strtok(NULL, "/");
 						}
-						strcpy(buf, "./filelist/"); // 서버의 filelist디렉토리에 저장하기 위해 경로 추가
-						strcat(buf, file_name); // 서버의 저장될 파일 경로 - buf
-
-						if((fp = fopen(buf, "w")) == NULL) { 
-							perror("file open");
-							exit(1);
-						}
-						fputs(file, fp); // 서버에 파일 저장
-						fclose(fp);
 						
-						if((fp = fopen("./filelist.txt", "a")) == NULL) {
-							perror("file open");
-							exit(1);
+						if(!fileexistcheck(file_name)) { // 파일명이 파일리스트에 존재하는가?
+							strcpy(buf, "exist");
+							if(send(clt_sock, buf, strlen(buf)+1, 0) == -1) { // 이미 존재할 경우 exist 전송
+								perror("send of sucess message");
+								exit(1);
+							}
 						}
-						fputs(file_name, fp); // 리스트에 파일 추가
-						fputs("\n", fp);
-						fclose(fp);
-						strcpy(buf, "upload completed\n");
-						if(send(clt_sock, buf, strlen(buf)+1, 0) == -1) { // 파일 저장 완료 메세지 전송
-							perror("send of sucess message");
-							exit(1);
-						}
+						else { // 파일명이 존재하지 않을 경우(중복 x) 파일 저장 수행
+							strcpy(buf, "./filelist/"); // 서버의 filelist디렉토리에 저장하기 위해 경로 추가
+							strcat(buf, file_name); // 서버의 저장될 파일 경로 - buf
 
+							if((fp = fopen(buf, "w")) == NULL) { 
+								perror("file open");
+								exit(1);
+							}
+							fputs(file, fp); // 서버에 파일 저장
+							fclose(fp);
+						
+							if((fp = fopen("./filelist.txt", "a")) == NULL) {
+								perror("file open");
+								exit(1);
+							}
+							fputs(file_name, fp); // 리스트에 파일 추가
+							fputs("\n", fp);
+							fclose(fp);
+							strcpy(buf, "upload completed\n");
+							if(send(clt_sock, buf, strlen(buf)+1, 0) == -1) { // 파일 저장 완료 메세지 전송
+								perror("send of sucess message");
+								exit(1);
+							}
+						}
 					} else if(!strcmp(buf, "download")) {
 						printf("download\n");
+						memset(buf, '\0', BUFSIZ);
+						filelist(buf); //파일 리스트 불러오기
+						if(send(clt_sock, buf, strlen(buf)+1, 0) == -1) { // 다운로드를 위해 파일 리스트를 새로 전송
+							perror("send of download message");
+							exit(1);
+						}
+						memset(buf, '\0', BUFSIZ);
+						if(recv(clt_sock, buf, sizeof(buf), 0) == -1) { // 클라이언트가 고른 파일명 받기
+							perror("recv of file");
+							exit(1);
+						}
+						printf("%s\n", buf);
+						memset(file, '\0', BUFSIZ);
+						strcpy(file, "./filelist/");
+						strcat(file, buf); // 파일명에 경로 추가
+						if((fp = fopen(file, "r")) == NULL) { // 서버에 있는 파일 오픈
+							perror("fopen");
+							exit(1);
+						}
+						memset(buf, '\0', BUFSIZ);
+						memset(file, '\0', BUFSIZ);
+						while((fread(file, sizeof(char), 1, fp)) > 0) { // 파일 내용 buf에 저장
+							strcat(buf, file);
+						}
+						if(send(clt_sock, buf, strlen(buf)+1, 0) == -1) { // 파일 내용 전송
+							perror("send of download message");
+							exit(1);
+						}
+						printf("The contents of file send completed...\n"); // 전송 완료
+						fclose(fp);
 					} else {
 						printf("not found\n");
 					}
@@ -152,4 +192,19 @@ void filelist(char* buf) {
 	}
 	
 	close(fd);
+}
+
+int fileexistcheck(char *filename) { // 파일리스트에 filename이 존재하는지 확인하는 메소드
+	char list[BUFSIZ] = {0, };
+	filelist(list);
+	char *file;
+	file = strtok(list, "\n"); 
+	while(file != NULL)
+		file = strtok(NULL, "\n");
+		if(!strcmp(filename, file)) {
+			printf("filename exists on the server\n");
+			return 0;
+		}
+	}
+	return 1;
 }
